b00b658816269463da7ec63ab67196cd30dd9f34
Bruno Mendes <102331277+BrunoSMendes89@users.noreply.github.com>
2022-04-12 19:42:47 -0300

    Update ConnectionExample1.java
    Changing the IP to not show here

--- src/ConnectionExample1.java
+++ src/ConnectionExample1.java
@@ -7,21 +7,21 @@ public class ConnectionExample1 {
 	public static void main(String[] args) {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			System.out.println("Driver de conex„o carregado.");
+			System.out.println("Driver de conex√£o carregado.");
 			
-			Connection cn =  DriverManager.getConnection("jdbc:mysql://192.168.15.86:3306",
+			Connection cn =  DriverManager.getConnection("jdbc:mysql://localhost:3306",
 					"usuario","senha");
 			
-			System.out.println("Conex„o aberta com sucesso.");
+			System.out.println("Conex√£o aberta com sucesso.");
 			
 			cn.close();
-			System.out.println("Conex„o encerrada.");
+			System.out.println("Conex√£o encerrada.");
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
-			System.out.println("N„o foi possÌvel encontrar o arquivo de conex„o.");
+			System.out.println("N√£o foi poss√≠vel encontrar o arquivo de conex√£o.");
 			e.printStackTrace();
 		} catch(SQLException sqle) {
-			System.out.println("N„o foi possÌvel abrir a conex„o.");
+			System.out.println("N√£o foi poss√≠vel abrir a conex√£o.");
 			sqle.printStackTrace();
 		}
 	}
